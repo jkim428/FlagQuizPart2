4eda964e8e565759ee4da15538d08acaee4e54bc
jkim428 <jkim428@student.cccd.edu>
2017-10-10 16:40:49 -0700

    10/10 In-class 4

--- app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.orangecoastcollege.cs273.flagquiz;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -225,12 +227,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Override onCreateOptionsMenu to inflate the settings menu
-
+    // Override onCreateOptionsMenu to inflate the settings menu within MainActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_settings, menu);
-
         return super.onCreateOptionsMenu(menu);
+    }
+
+    // Respond to the user clicking the Settings menu icon
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Make a new Intent going to SettingsActivity
+        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+        startActivity(settingsIntent);
+
+        return super.onOptionsItemSelected(item);
     }
 }
